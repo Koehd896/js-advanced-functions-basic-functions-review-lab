88b63ce39ee6515f080c43449347245fa9323aa5
Raven <ravenkoehler@gmail.com>
2021-09-11 18:06:40 -0400

    commit tree cc8b0ab8d68d2f9fab1769e07dc42eae29649799

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function saturdayFun(word = "roller-skate") {
+    return `This Saturday, I want to ${word}!`;
+}
+
+const mondayWork = function(word = "go to the office") {
+    return `This Monday, I will ${word}.`
+} 
+
+function wrapAdjective(flair = "*") {
+    return function(param = "special") {
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2;
+    },
+    divide: function(num1, num2) {
+        return num1 / num2;
+    }
+ }
+
+ function actionApplyer(start, fncArray) {
+    function reducer(previous, current){
+        return current(previous);
+    }
+    return fncArray.reduce(reducer, start);
+ }
